e6bb3b1a4c767b4fe61ea860e2c00c49b6d3c682
Hemant Chaurasia <62471000+hemantchaurasia2002@users.noreply.github.com>
2021-05-03 21:08:09 +0530

    2nd commit

---
+++ rec.cpp
@@ -0,0 +1,27 @@
+/*#include <iostream>
+
+using namespace std;
+
+void area
+{   int l=7,b=5;
+    float area;
+    area=(float(l+b)/2); 
+ cout<<area;
+}*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int b,h;
+    cin>>b;
+    cin>>h;
+   
+    float area;
+	
+    area=(float(b * h)/2);
+    
+ 
+ cout<<area;
+}
